ed55b794cf98d104dcf13857affee571e3a49bc3
Luyi Bai <Pnctsrc@users.noreply.github.com>
2017-06-24 20:31:47 -0400

    Stay on the same page for invalid page number

--- pages/posts/client/posts.js
+++ pages/posts/client/posts.js
@@ -24,12 +24,13 @@ Template.posts.events({
 
   "change .page_num": function(){
     const text = $("input.page_num").val();
+    const current_page = parseInt(Router.current().params.page_num);
     const total_pages = Math.ceil(MetaData.findOne().total_count / MetaData.findOne().posts_per_page);
     var final_page_num;
 
     if(!/^\d+$/i.test(text)){
-      $("input.page_num").val(1);
-      final_page_num = 1;
+      $("input.page_num").val(current_page);
+      final_page_num = current_page;
     } else if (parseInt(text) > total_pages){
       $("input.page_num").val(total_pages);
       final_page_num = total_pages;
@@ -44,12 +45,13 @@ Template.posts.events({
   "keypress .page_num": function(event){
     if(event.which === 13){
       const text = $("input.page_num").val();
+      const current_page = parseInt(Router.current().params.page_num);
       const total_pages = Math.ceil(MetaData.findOne().total_count / MetaData.findOne().posts_per_page);
       var final_page_num;
 
       if(!/^\d+$/i.test(text)){
-        $("input.page_num").val(1);
-        final_page_num = 1;
+        $("input.page_num").val(current_page);
+        final_page_num = current_page;
       } else if (parseInt(text) > total_pages){
         $("input.page_num").val(total_pages);
         final_page_num = total_pages;
