0e087dddb3202728ae88dfca511a10d8983460a4
Luyi Bai <Pnctsrc@users.noreply.github.com>
2017-06-27 14:58:04 -0400

    Fixed bold effect for link buttons

--- client/layout.js
+++ client/layout.js
@@ -1,6 +1,6 @@
 Template.layout.events({
-  "click #buttons_list > a": function(event){
-    $("#buttons_list a").removeClass("active");
+  "click #link_list > a": function(event){
+    $("#link_list a").removeClass("active");
     $("#collapse_button ul a").removeClass("active");
     $(event.currentTarget).addClass("active");
     const link_type = event.currentTarget.innerHTML.toLowerCase();
@@ -9,9 +9,9 @@ Template.layout.events({
 
   "click #collapse_button ul a": function(event){
     $("#collapse_button ul a").removeClass("active");
-    $("#buttons_list a").removeClass("active");
+    $("#link_list a").removeClass("active");
     $(event.currentTarget).addClass("active");
     const link_type = event.currentTarget.innerHTML.toLowerCase();
-    $("#buttons_list a[class=" + link_type + "_link]").addClass("active");
+    $("#link_list a[class=" + link_type + "_link]").addClass("active");
   },
 })
