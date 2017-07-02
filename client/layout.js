e9942b8b313436b3513f96afaa6f97234125a035
Luyi Bai <Pnctsrc@users.noreply.github.com>
2017-07-02 04:09:39 -0400

    Set active nav bar link after page load

--- client/layout.js
+++ client/layout.js
@@ -1,3 +1,22 @@
+Template.layout.onRendered(function(){
+  var page_type = Router.current().url.match(/(work|post|about)/);
+  if(!page_type) return;
+
+  var link_type;
+  if(page_type[0] === "post"){
+    link_type = "blog";
+  } else if (page_type[0] === "work"){
+    link_type = "works";
+  } else if (page_type[0] === "about"){
+    link_type = "about";
+  }
+
+  $("#link_list a").removeClass("active");
+  $("#collapse_button ul a").removeClass("active");
+  $("#collapse_button ul a[class=" + link_type + "_link]").addClass("active");
+  $("#link_list a[class=" + link_type + "_link]").addClass("active");
+})
+
 Template.layout.events({
   "click #link_list > a": function(event){
     $("#link_list a").removeClass("active");
