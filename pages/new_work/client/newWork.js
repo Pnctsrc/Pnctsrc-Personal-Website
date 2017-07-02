58b01eb3cbc0d18753762e569384574ec2406ea5
Luyi Bai <Pnctsrc@users.noreply.github.com>
2017-07-02 01:28:38 -0400

    Works thumbnail

--- pages/new_work/client/newWork.js
+++ pages/new_work/client/newWork.js
@@ -57,7 +57,10 @@ Template.newWork.helpers({
 })
 
 Template.newWork.events({
-  "click #work_submit": function(){
+  "click #work_submit": function(event){
+    //set loading status of the button
+    $("#work_submit").attr("class", "ui right floated blue loading button");
+
     const access_key = Router.current().params.hash;
     const submit_object = {
       HTML_content: $('#froala-editor').froalaEditor('html.get', true),
@@ -67,15 +70,49 @@ Template.newWork.events({
       type: $("#work_type").dropdown("get value")
     };
 
-    Meteor.call("submit_work", submit_object, access_key,function(err, result){
-      if(err){
-        window.alert(err);
-        Router.go("/works?page=1");
+    //client-side validation
+    if($("#work_pic").val()){
+      const file_type = $("#work_pic")[0].files[0].type;
+      if(file_type.substring(0, 5) !== "image"){
+        window.alert("Please upload an image as the thumbnail.");
         return;
       }
 
-      Router.go("/works/view/" + result);
-    });
+      //get the image file and convert it to base64 form
+      var image_file = $("#work_pic")[0].files[0];
+      var imgToBase64 = function(image_file, callback) {
+        var reader = new FileReader();
+        reader.onload = function() {
+          var dataUrl = reader.result;
+          var img_base64 = dataUrl.split(',')[1];
+          callback(img_base64);
+        };
+        reader.readAsDataURL(image_file);
+      };
+
+      //send the data to the sever
+      imgToBase64(image_file, function(image_base64){
+        Meteor.call("submit_work", submit_object, access_key, image_base64, file_type, function(err, result){
+          if(err){
+            window.alert(err.message);
+            $("#work_submit").attr("class", "ui right floated blue button");
+            return;
+          }
+
+          Router.go("/works/view/" + result);
+        });
+      })
+    } else {
+      Meteor.call("submit_work", submit_object, access_key, function(err, result){
+        if(err){
+          window.alert(err.message);
+          $("#work_submit").attr("class", "ui right floated blue button");
+          return;
+        }
+
+        Router.go("/works/view/" + result);
+      });
+    }
   },
 
   "change #work_pic": function(event){
--- pages/new_work/server/methods.js
+++ pages/new_work/server/methods.js
@@ -1,30 +1,85 @@
 Meteor.methods({
-  "submit_work": function(object, access_key){
+  "submit_work": function(object, access_key, image_base64, file_type){
     //server-side validation
     if(access_key !== Meteor.settings.PNCTSRC_ACCESS_KEY){
       console.log("key - " + access_key);
       throw new Meteor.Error(100, "Access denied");
     }
 
-    //insert into database
-    const _id = Works.insert({
-        HTML_content: object.HTML_content,
-        title: object.title,
-        description: object.description,
-        view_count: 0,
-        date: object.date,
-        type: object.type,
-        createdAt: new Date()
-    })
+    if(!image_base64){
+      //insert into database
+      const _id = Works.insert({
+          HTML_content: object.HTML_content,
+          title: object.title,
+          description: object.description,
+          view_count: 0,
+          date: object.date,
+          type: object.type,
+          thumbnail: "",
+          createdAt: new Date()
+      })
 
-    MetaData.update({
-      type: "works"
-    }, {
-      $set: {
-        total_count: MetaData.findOne({type: "works"}).total_count + 1
-      }
-    })
+      MetaData.update({
+        type: "works"
+      }, {
+        $set: {
+          total_count: MetaData.findOne({type: "works"}).total_count + 1
+        }
+      })
 
-    return _id;
-  }
+      return _id;
+    } else {
+      return Meteor.call("upload_thumbnail_to_S3", object, image_base64, file_type);
+    };
+  },
+
+  "upload_thumbnail_to_S3": function(object, image_base64, file_type){
+    //convert base64 to image_file blob
+    var image_file = new Buffer(image_base64, 'base64');
+
+    // Import the Amazon S3 service client
+    var S3 = Npm.require('aws-sdk/clients/s3');
+
+    // Set credentials and region
+    var s3 = new S3({
+        apiVersion: '2006-03-01',
+        region: 'us-east-1',
+        accessKeyId: Meteor.settings.AWS_ACCESS_KEY,
+        secretAccessKey: Meteor.settings.AWS_SECRET_KEY,
+    });
+
+    var params = {
+      Bucket: Meteor.settings.S3_BUCKET_NAME,
+      Key: Meteor.settings.S3_PATH_NAME + "images/thumbnail_" + (new Date()).getTime() + "." + file_type.substring(6),
+      ACL: "public-read",
+      Body: image_file
+    };
+
+    return s3.upload(params).promise().then(function(data) {
+      const img_URL = data.Location;
+      //insert into database
+      const _id = Works.insert({
+          HTML_content: object.HTML_content,
+          title: object.title,
+          description: object.description,
+          view_count: 0,
+          date: object.date,
+          type: object.type,
+          thumbnail: img_URL,
+          createdAt: new Date()
+      })
+
+      MetaData.update({
+        type: "works"
+      }, {
+        $set: {
+          total_count: MetaData.findOne({type: "works"}).total_count + 1
+        }
+      })
+
+      return _id;
+    }).catch(function(err) {
+      throw new Meteor.Error(100, err);
+    });
+  },
 })
