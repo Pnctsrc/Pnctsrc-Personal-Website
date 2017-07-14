dd87a8c864aee407bcf7e57212cb322abedbe4c5
Luyi Bai <Pnctsrc@users.noreply.github.com>
2017-07-13 23:17:12 -0400

    Display createdAt of posts in YYYY-MM-DD format

--- pages/posts/client/post_view.js
+++ pages/posts/client/post_view.js
@@ -59,7 +59,13 @@ Template.postView.helpers({
       return "";
     }
 
-    return $.timeago(date);
+    function convertDate(inputFormat) {
+      function pad(s) { return (s < 10) ? '0' + s : s; }
+      var d = new Date(inputFormat);
+      return [d.getFullYear(), pad(d.getMonth()+1), pad(d.getDate())].join('-');
+    }
+
+    return convertDate(date);
   },
 
   "postViewDict": function(){
--- pages/posts/client/posts.js
+++ pages/posts/client/posts.js
@@ -9,7 +9,13 @@ Template.posts.helpers({
   },
 
   "getTime": function(date){
-      return $.timeago(date) || "";
+    function convertDate(inputFormat) {
+      function pad(s) { return (s < 10) ? '0' + s : s; }
+      var d = new Date(inputFormat);
+      return [d.getFullYear(), pad(d.getMonth()+1), pad(d.getDate())].join('-');
+    }
+
+    return convertDate(date);
   },
 
   "dataNotReady": function(){
