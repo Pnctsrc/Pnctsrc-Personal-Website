4dc22308b4072e231c3ac362b80062c6f05a72b0
Luyi Bai <Pnctsrc@users.noreply.github.com>
2017-07-22 22:20:36 -0400

    Server side validation for image upload

--- server/api/config/api.js
+++ server/api/config/api.js
@@ -37,14 +37,37 @@ API = {
         var busboy = new Busboy({headers: context.request.headers});
 
         var final_filename;
+        var ifError = false;//true if any error happens later
         busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-          const fileExtension = "." + mimetype.substring(6).replace("e", "");
-          final_filename = (new Date()).getTime() + fileExtension;
+          const fileExtension = mimetype.substring(6).replace("e", "");
+
+          //validate the file
+          var file_size = 0;
+          file.on('data', function(data){
+            file_size += data.length;
+
+            if(file_size > 10485760){
+              API.utility.sendError(context, 400, "Image size larger than 10MB.");
+              ifError = true;
+            }
+          })
+
+          if(_.indexOf(["jpg", "jpeg", "png"], fileExtension) === -1){
+            API.utility.sendError(context, 400, "Image type not allowed.");
+            ifError = true;
+          }
+
+          if(ifError) return;
+
+          final_filename = (new Date()).getTime() + "." + fileExtension;
           var saveTo = Meteor.settings.IMAGE_PATH + final_filename;
+
           file.pipe(fs.createWriteStream(saveTo));
         });
 
         busboy.on('finish', function() {
+          if(ifError) return;
+
           API.utility.response(context, 200, {link: "http://localhost:3000/resources/images/" + final_filename});
         });
 
@@ -76,6 +99,12 @@ API = {
     },
     validate: function(data, pattern){
       return Match.test(data, pattern);
+    },
+    sendError: function(context, statusCode, message){
+      API.utility.response(context, statusCode, {
+        error: 400,
+        message: message
+      });
     }
   }
 };
