8130104d397defab29435514a999363127865b5f
Luyi Bai <Pnctsrc@users.noreply.github.com>
2017-07-31 22:58:43 -0400

    New check for active nav links

--- client/layout.js
+++ client/layout.js
@@ -1,44 +1,11 @@
-Template.layout.onRendered(function(){
-  var page_type = Router.current().url.match(/(work|post|about)/);
-  if(!page_type) return;
-
-  var link_type;
-  if(page_type[0] === "post"){
-    link_type = "blog";
-  } else if (page_type[0] === "work"){
-    link_type = "works";
-  } else if (page_type[0] === "about"){
-    link_type = "about";
-  }
-
-  $("#link_list a").removeClass("active");
-  $("#collapse_button ul a").removeClass("active");
-  $("#collapse_button ul a[class=" + link_type + "_link]").addClass("active");
-  $("#link_list a[class=" + link_type + "_link]").addClass("active");
-})
-
-Template.layout.events({
-  "click #link_list > a": function(event){
-    $("#link_list a").removeClass("active");
-    $("#collapse_button ul a").removeClass("active");
-    $(event.currentTarget).addClass("active");
-    const link_type = event.currentTarget.innerHTML.toLowerCase();
-    $("#collapse_button ul a[class=" + link_type + "_link]").addClass("active");
-  },
-
-  "click #collapse_button ul a": function(event){
-    $("#collapse_button ul a").removeClass("active");
-    $("#link_list a").removeClass("active");
-    $(event.currentTarget).addClass("active");
-    const link_type = event.currentTarget.innerHTML.toLowerCase();
-    $("#link_list a[class=" + link_type + "_link]").addClass("active");
-  },
-})
-
 Template.layout.helpers({
   "homepage": function(){
     return Homepage.findOne();
   },
+  "ifActive": function(name){
+    const page_type = Router.current().route._path.match(/(works|posts|about|me)/)[0];
+    return name === page_type ? "active" : "";
+  }
 })
 
 Template.layout.events({
