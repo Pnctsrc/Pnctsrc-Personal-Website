6ed17f3baf9cce244e10bf993d8073f78f1e99b0
Luyi Bai <Pnctsrc@users.noreply.github.com>
2017-08-01 22:33:26 -0400

    Fetch usernames in comment publication

--- server/publications.js
+++ server/publications.js
@@ -3,7 +3,47 @@ Meteor.publish({
     return Homepage.find();
   },
   "comments": function(post_id){
-    return Comments.find({post_id: post_id});
+    const self = this;
+    Comments.find({post_id: post_id}).forEach(function(comment){
+      const comment_id = comment._id;
+      const profile = Meteor.users.findOne(comment.userId);
+      const first_name = profile.pnctsrc.first_name;
+      const last_name = profile.pnctsrc.last_name;
+      const username = (first_name + " " + last_name).replace(/ +/gi, " ").trim();
+      if(/^ +$/gi.test(username)){
+        comment.username = "Member";
+        self.added("comments", comment_id, comment);
+      } else {
+        comment.username = username;
+        self.added("comments", comment_id, comment);
+      }
+    });
+
+    var handle = Comments.find({post_id: post_id}).observeChanges({
+    added: function(id) {
+      const comment = Comments.findOne(id);
+      const profile = Meteor.users.findOne(comment.userId);
+      const first_name = profile.pnctsrc.first_name;
+      const last_name = profile.pnctsrc.last_name;
+      const username = (first_name + " " + last_name).replace(/ +/gi, " ").trim();
+      if(/^ +$/gi.test(username)){
+        comment.username = "Member";
+        self.added("comments", id, comment);
+      } else {
+        comment.username = username;
+        self.added("comments", id, comment);
+      }
+    },
+    changed: function(id, fields){
+      self.changed("comments", id, fields);
+    },
+    removed: function(id){
+      self.removed("comments", id);
+    }
+  })
+
+    this.ready();
+    this.onStop(() => handle.stop());
   },
   "user": function(){
     return Meteor.users.find({
