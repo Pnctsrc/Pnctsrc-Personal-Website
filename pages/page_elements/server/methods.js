b4a6a0e54df9666134116c783016665f1c945403
Luyi Bai <Pnctsrc@users.noreply.github.com>
2017-08-01 20:53:55 -0400

    Validate comment insersion

--- pages/page_elements/server/methods.js
+++ pages/page_elements/server/methods.js
@@ -1,5 +1,11 @@
 Meteor.methods({
   "insert_comment": function(comment){
+    //validate
+    if(!this.userId){
+      throw new Meteor.Error("403", "Not logged in.");
+    }
+
+    comment.userId = this.userId;
     Comments.insert(comment);
   }
 })
