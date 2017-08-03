5d83b7be62746df3b6185e990fb221de88b47554
Luyi Bai <Pnctsrc@users.noreply.github.com>
2017-08-03 10:47:35 -0400

    Notifications page

--- pages/me/client/me.js
+++ pages/me/client/me.js
@@ -2,6 +2,12 @@ Template.me.helpers({
   "ifActive": function(name){
     const page_type = Router.current().route._path.match(/(profile|notifications)/)[0];
     return name === page_type ? "active" : "";
+  },
+  "profilePage": function(){
+    return Router.current().route._path.match(/profile/);
+  },
+  "notificationsPage": function(){
+    return Router.current().route._path.match(/notifications/);
   }
 })
 
