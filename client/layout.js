54b1ee8ea36fe2a1104046742a3561b4c0d8bf38
Luyi Bai <Pnctsrc@users.noreply.github.com>
2017-08-06 20:38:46 -0400

    Ignore new work/post for active nav links

--- client/layout.js
+++ client/layout.js
@@ -3,7 +3,7 @@ Template.layout.helpers({
     return Homepage.findOne();
   },
   "ifActive": function(name){
-    if(!Router.current().route) return;
+    if(!Router.current().route || Router.current().route._path.match(/(post_new|work_new)/)) return;
 
     const page_type = Router.current().route._path.match(/(works|posts|about|me)/)[0];
     return name === page_type ? "active" : "";
