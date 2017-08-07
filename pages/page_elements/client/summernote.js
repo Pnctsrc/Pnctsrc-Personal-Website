37e291053b16c939af48f12887c969a5baa5fd6a
Luyi Bai <Pnctsrc@users.noreply.github.com>
2017-08-06 20:16:44 -0400

    Sticky Summernote toolbar

--- pages/page_elements/client/summernote.js
+++ pages/page_elements/client/summernote.js
@@ -13,6 +13,44 @@ Template.summernote.helpers({
 
 Template.summernote_editor.onCreated(function(){
   this.editorDict = this.data.editorDict;
+
+  //attach sticky toolbar listener
+  $(window).scroll(function(){
+    const page_top = $(window).scrollTop();
+    const editor_top = $(".note-editor")[0].offsetTop;
+    const toolbar_top = editor_top - page_top;
+    const $toolbar = $(".note-toolbar");
+    const $editor = $(".note-editor");
+    const $editing_area = $(".note-editing-area");
+
+    if(toolbar_top <= 0){
+      const editor_width = $editor.width();
+      $toolbar.css("position", "fixed");
+      $toolbar.css("top", "0");
+      $toolbar.css("width", editor_width);
+      $toolbar.css("z-index", 1);
+      $editing_area.css("z-index", 0);
+    } else {
+      $toolbar.css("position", "");
+      $toolbar.css("top", "");
+      $toolbar.css("width", "");
+      $toolbar.css("z-index", "");
+      $editing_area.css("z-index", "");
+    }
+  })
+
+  $(window).resize(function(){
+    const page_top = $(window).scrollTop();
+    const editor_top = $(".note-editor")[0].offsetTop;
+    const toolbar_top = editor_top - page_top;
+
+    if(toolbar_top <= 0){
+      const $editor = $(".note-editor");
+      const $toolbar = $(".note-toolbar");
+      const editor_width = $editor.width();
+      $toolbar.css("width", editor_width);
+    }
+  })
 })
 
 Template.summernote_editor.onRendered(function(){
